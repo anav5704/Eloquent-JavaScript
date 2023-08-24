bb76afde504fe496a469d629767756fd550fac10
Anav Chand <anavkki@gmail.com>
2023-08-24 22:50:10 +1200

    JavaScript :)

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Hello World 👋
+
+Um so this is like my JavaScript arc. I'll be adding cool stuff I learn in here.
---
+++ Values.js
@@ -0,0 +1,32 @@
+// Nan
+console.log(Infinity - Infinity)
+console.log(0/0)
+console.log(NaN ==  NaN) // false
+
+
+// the \ bullshit from c++
+console.log("Hello world \n Im \t Anav")
+
+
+// template literals
+console.log(`I'm ${2023 - 2004} years old`)
+
+
+// unary operators
+console.log(typeof("Anav") == Number)
+
+
+// more bullshit
+console.log("A" > "a") // false
+
+
+// my bae, ternary operators
+raining = true
+console.log(raining ? "Take an umbrella" : "Wear Shorts") // false
+
+
+// JavaScript being JavaScript
+console.log("5" - 1) // 4
+console.log("5" + 1) // 51
+
+// 🔍 short circuiting logical operators
