5e8a1f5244b96e528c077367dc9112ace707c013
Anav Chand <anavkki@gmail.com>
2023-08-26 10:42:42 +1200

    :)

--- Structure.js
+++ Structure.js
@@ -1,34 +1,44 @@
 // JavaScript Supremacy
-for(let line = " * "; line.length < 24; line += " * "){
-    console.log(line)
-}
+// for(let line = " * "; line.length < 24; line += " * "){
+//     console.log(line)
+// }
 
 // Fizz Buzz
-for(let i = 1; i <= 100; i++){
-    i % 3 == 0 && i % 5 == 0 ? console.log("FizzBuzz") 
-    :
-    i % 3 == 0 && i % 5 != 0 ? console.log("Fizz") 
-    : 
-    i % 5 == 0 && i % 3 != 0 ? console.log("Buzz") 
-    : 
-    console.log(i)
-}
+// for(let i = 1; i <= 100; i++){
+//     i % 3 == 0 && i % 5 == 0 ? console.log("FizzBuzz") 
+//     :
+//     i % 3 == 0 && i % 5 != 0 ? console.log("Fizz") 
+//     : 
+//     i % 5 == 0 && i % 3 != 0 ? console.log("Buzz") 
+//     : 
+//     console.log(i)
+// }
 
 // Fizz Buzz but better
-for(let i = 1; i <= 100; i++){
-    let output = ""
+// for(let i = 1; i <= 100; i++){
+//     let output = ""
 
-    if(i % 5 == 0) output += "Fizz"
-    if(i % 3 == 0) output += "Buzz"
+//     if(i % 5 == 0) output += "Fizz"
+//     if(i % 3 == 0) output += "Buzz"
 
-    console.log(output || i)
-}
+//     console.log(output || i)
+// }
 
 let size = 8
 let chessBoard = ""
 
 for(let i = 0; i < size; i++){
-   //
+    if (i % 2 == 0) {
+        for(let i = 0; i < size; i++){
+            chessBoard += " #"
+        }
+    }
+    else{
+        for(let i = 0; i < size; i++){
+            chessBoard += "# "
+        }
+    }
+    chessBoard += "\n"
 }
 
 console.log(chessBoard)
