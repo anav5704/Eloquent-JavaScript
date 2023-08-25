55dd2ed88f2bcd1ecea4ef819fcfd0a57f8b59ae
Anav Chand <anavkki@gmail.com>
2023-08-26 10:43:29 +1200

    Eloquent

--- Structure.js
+++ Structure.js
@@ -1,29 +1,30 @@
 // JavaScript Supremacy
-// for(let line = " * "; line.length < 24; line += " * "){
-//     console.log(line)
-// }
+for(let line = " * "; line.length < 24; line += " * "){
+    console.log(line)
+}
 
 // Fizz Buzz
-// for(let i = 1; i <= 100; i++){
-//     i % 3 == 0 && i % 5 == 0 ? console.log("FizzBuzz") 
-//     :
-//     i % 3 == 0 && i % 5 != 0 ? console.log("Fizz") 
-//     : 
-//     i % 5 == 0 && i % 3 != 0 ? console.log("Buzz") 
-//     : 
-//     console.log(i)
-// }
+for(let i = 1; i <= 100; i++){
+    i % 3 == 0 && i % 5 == 0 ? console.log("FizzBuzz") 
+    :
+    i % 3 == 0 && i % 5 != 0 ? console.log("Fizz") 
+    : 
+    i % 5 == 0 && i % 3 != 0 ? console.log("Buzz") 
+    : 
+    console.log(i)
+}
 
 // Fizz Buzz but better
-// for(let i = 1; i <= 100; i++){
-//     let output = ""
+for(let i = 1; i <= 100; i++){
+    let output = ""
 
-//     if(i % 5 == 0) output += "Fizz"
-//     if(i % 3 == 0) output += "Buzz"
+    if(i % 5 == 0) output += "Fizz"
+    if(i % 3 == 0) output += "Buzz"
 
-//     console.log(output || i)
-// }
+    console.log(output || i)
+}
 
+// Le chess board
 let size = 8
 let chessBoard = ""
 
